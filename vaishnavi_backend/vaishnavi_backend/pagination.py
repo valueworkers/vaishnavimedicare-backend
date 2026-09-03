7179447d851c5797a45c629e7c188e7b39627cd2
Pankaj Shinde <pankaj.shinde.codes@gmail.com>
2026-09-03 12:38:36 +0530

    fix(pagination): Restore max_page_size attribute in StandardResultsSetPagination

--- vaishnavi_backend/vaishnavi_backend/pagination.py
+++ vaishnavi_backend/vaishnavi_backend/pagination.py
@@ -6,6 +6,7 @@ import math
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
+    max_page_size = 1000
 
     def get_paginated_response(self, data):
         total_items = self.page.paginator.count
